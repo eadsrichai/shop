ce52bb4ced51c8da9ee63a3f34875f33f5705d96
Wuttiowong Eadsrichai <wutkmutt@hotmail.com>
2023-08-26 11:30:18 +0700

    update edit_product_by_id

--- back-end/edit_product_by_id.php
+++ back-end/edit_product_by_id.php
@@ -1,15 +1,17 @@
 <?php
     include_once('db.php');
 
-    $sql = "UPDATE product SET p_name=? , p_price=?, p_detail=?
+    $sql = "UPDATE product 
+            SET p_name=? , p_price=?, p_detail=?, t_id=?
             WHERE p_id =?";
     
     $stmt = $conn->prepare($sql);
 
-    $stmt->bind_param("ssss", $name,$price,$detail,$id);
+    $stmt->bind_param("sssss", $name,$price,$detail,$t_id,$id);
     $name = $_GET['p_name'];
     $price= $_GET['p_price'];
     $detail = $_GET['p_detail'];
+    $t_id = $_GET['t_id'];
     $id= $_GET['p_id'];
     
     $stmt->execute();
